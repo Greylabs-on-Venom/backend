6bd2dec5f72d460a7bdce26cf03417aff247a192
Ouches Technology <henryhambolu@gmail.com>
2023-11-12 21:32:23 +0100

    updated auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,35 +2,37 @@ const crypto = require('crypto');
 const User = require('../models/user.model'); // Import the User model
 const jwt = require("jsonwebtoken");
 const config = require('../config/config');
-// Signup with Twitter profile
-// function generateRandomId(length) {
-//   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//   let result = '';
-//   const charactersLength = characters.length;
-//   for (let i = 0; i < length; i++) {
-//     const randomIndex = Math.floor(Math.random() * charactersLength);
-//     result += characters.charAt(randomIndex);
-//   }
-//   return result;
-// }
+
 
 exports.signup = async (req, res) => {
   try {
-    // let uniqueID;
-    // let isUniqueIdTaken = true;
 
-    // // Keep generating a unique ID until one is found that doesn't already exist
-    // while (isUniqueIdTaken) {
-    //   uniqueID = generateRandomId(8);
-    //   const existingUser = await User.findOne({ uniqueID });
-    //   isUniqueIdTaken = !!existingUser;
-    // }
+    const user = await User.findOne({
+      venomAddress: req.body.venomAddress,
+    });
 
+    if (user) {
+      return res.status(404).json({ message: 'User already exsist' });
+    }
+
+    function generateRandomString(length) {
+      const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let result = '';
+      
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        result += charset.charAt(randomIndex);
+      }
+    
+      return result;
+    }
+    
+    const maxLength = 10; // Change this to your desired maximum length
+    const randomString = generateRandomString(maxLength);
+    
     // Create a new user with the unique ID
     const newUser = new User({
-      login: 'GLAB' + req.body.discordProfile,
-      // twitterProfile: req.body.twitterProfile,
-      // discordProfile: req.body.discordProfile,
+      login: 'GLAB' + randomString,
       venomAddress: req.body.venomAddress
     });
 
@@ -73,7 +75,7 @@ exports.login = async (req, res) => {
         user:user
      });
   } catch (error) {
-    console.error(error);
+    //console.error(error);
     res.status(500).json({ 
       message: 'Internal Server Error',
       error: error
