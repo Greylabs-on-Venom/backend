3f33f268c6dadd8c5d570ae1c900f443e010e036
Ouches Technology <henryhambolu@gmail.com>
2023-12-16 00:20:25 +0100

    updated

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -9,7 +9,7 @@ require('dotenv').config({path: './.env'})
 
 
 verifyToken = (req, res, next) => {
-  const token = req.headers('Authorization');
+  const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized access' });
